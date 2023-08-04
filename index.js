51434683718a8da9b60387d4249dcf0969a54bb6
SalmanHersi <hersi.salman@gmail.com>
2023-08-04 09:33:20 -0400

    draft

---
+++ index.js
@@ -0,0 +1,27 @@
+const { fetchMyIP } = require("./iss");
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned IP:", ip);
+});
+
+const fetchMyIP = function (callback) {
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(
+        Error(`Status Code ${response.statusCode} when fetching IP: ${body}`),
+        null
+      );
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
